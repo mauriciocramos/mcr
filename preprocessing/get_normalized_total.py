d67ff506e615611a50adbc792156e60f4874a06d
mauriciocramos <mauriciocramos@gmail.com>
2020-06-30 14:12:42 -0300

    Refactoring argument and variable names Adding docstring

--- preprocessing/get_normalized_total.py
+++ preprocessing/get_normalized_total.py
@@ -2,21 +2,33 @@ import numpy as np
 import pandas as pd
 
 
-def get_normalized_total(df, reference, total):
-    # df = df[['FTE', 'Total']].copy()
-    # df['FTE_missing'] = df.FTE.isnull().astype('uint8')
-    # df['Total_missing'] = df.Total.isnull().astype('uint8')
-    # df['Total_FTE'] = np.nan
-    # df.loc[df.FTE.notnull() & df.FTE != 0, 'Total_FTE'] = df.Total
-    # df.loc[df.FTE.notnull() & df.FTE != 0, 'Total'] = np.nan
-    # df[['FTE', 'Total', 'Total_FTE']] = df[['FTE', 'Total', 'Total_FTE']].apply(pd.to_numeric, downcast='float')
-    # return df
-    df = df[[reference, total]].copy()
+def get_normalized_total(df, reference, ambiguous):
+    """
+    Feature generation function which generates missing data binary indicators for two numeric features, and moves
+    values from an ambiguous column to a new column, based on whether a reference column is not missing or
+    is different than zero.
+
+    The missing data binary indicators are named with the former reference and ambiguous column names appended with the
+    prefix '_missing'.
+
+    The ambiguous values related to the non-zero reference values are moved to a new column appended with the reference
+    column name.  After moving the value, the original columns is filled with a missing value NaN.
+
+    All resulting missing values in the 'reference', 'ambiguous' and 'ambiguous_reference' columns are imputed with zero
+    with the intent to promote sparsity.  Finally, those column types are downcast to the smallest float possible.
+
+    :param df: a dataframe
+    :param reference: the reference column
+    :param ambiguous: the ambiguous columns
+    :return: a dataframe (copy) with the three new columns and the moved/imputed values
+    """
+    df = df[[reference, ambiguous]].copy()
     df[reference+'_missing'] = df[reference].isnull().astype('uint8')
-    df[total+'_missing'] = df[total].isnull().astype('uint8')
-    total_reference = total+'_'+reference
-    df[total_reference] = np.nan
-    df.loc[df[reference].notnull() & df[reference] != 0, total_reference] = df[total]
-    df.loc[df[reference].notnull() & df[reference] != 0, total] = np.nan
-    df[[reference, total, total_reference]] = df[[reference, total, total_reference]].apply(pd.to_numeric, downcast='float')
+    df[ambiguous+'_missing'] = df[ambiguous].isnull().astype('uint8')
+    ambiguous_reference = ambiguous+'_'+reference
+    df[ambiguous_reference] = np.nan
+    df.loc[df[reference].notnull() & df[reference] != 0, ambiguous_reference] = df[ambiguous]
+    df.loc[df[reference].notnull() & df[reference] != 0, ambiguous] = np.nan
+    df[[reference, ambiguous, ambiguous_reference]] = \
+        df[[reference, ambiguous, ambiguous_reference]].apply(pd.to_numeric, downcast='float')
     return df
