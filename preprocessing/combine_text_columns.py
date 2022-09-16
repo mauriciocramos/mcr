7c595003ef1f1b05a951bb2d7a1318990c54f096
Maurício Collaça <mauriciocramos@gmail.com>
2022-09-16 08:20:56 -0300

    smarter automatic filter

--- preprocessing/combine_text_columns.py
+++ preprocessing/combine_text_columns.py
@@ -3,11 +3,16 @@ def combine_text_columns(df=None, to_drop=None, sep=' '):
         then combines all of the text columns into a single vector that has all of
         the text for a row.
 
-        :param data_frame: The data as read in with read_csv (no preprocessing necessary)
+        :param df: The data as read in with read_csv (no preprocessing necessary)
         :param to_drop: (optional) Removes the numeric columns by default.
+        :param sep: Default ' '. Could be '. ' for sentence tokenizing.
     """
-    # drop non-text columns that are in the df
+   
     if to_drop is None:
+        # drop non-text columns that are in the df
         to_drop = df.columns[df.dtypes != 'object']
-    to_drop = set(to_drop) & set(df.columns)
+    else:
+        # drop selected plus non-object
+        to_drop = (set(to_drop) & set(df.columns)) | set(df.columns[df.dtypes != 'object'])
+    
     return df.drop(to_drop, axis=1).apply(lambda x: x.str.cat(sep=sep), axis=1)
