f7eeebee68283ba38366f82d66b88933923476fc
Maurício Collaça <mauriciocramos@gmail.com>
2023-01-26 20:22:26 -0300

    networkx

--- mcr/nx/nx.py
+++ mcr/nx/nx.py
@@ -102,59 +102,51 @@ def path_exists(G, node1, node2):
 
 
 def find_nodes_with_highest_degree_centrality(G):
+    # Find the node(s) that has the highest degree centrality in a graph
+    # top_dc = find_nodes_with_highest_deg_cent(G)
+    # print(top_dc)
+    # Write the assertion statement
+    # for node in top_dc:
+    #     assert nx.degree_centrality(G)[node] == max(nx.degree_centrality(T).values())
+    
     # Compute the degree centrality of G: deg_cent
     deg_cent = nx.degree_centrality(G)
 
     # Compute the maximum degree centrality: max_dc
     max_dc = max(list(deg_cent.values()))
-
     nodes = set()
-
     # Iterate over the degree centrality dictionary
     for k, v in deg_cent.items():
-
         # Check if the current value has the maximum degree centrality
         if v == max_dc:
             # Add the current node to the set of nodes
             nodes.add(k)
-
     return nodes
 
 
-# # Find the node(s) that has the highest degree centrality in T: top_dc
-# top_dc = find_nodes_with_highest_deg_cent(T)
-# print(top_dc)
-# # Write the assertion statement
-# for node in top_dc:
-#     assert nx.degree_centrality(T)[node] == max(nx.degree_centrality(T).values())
-
-
 def find_node_with_highest_betweenness_centrality(G):
+    # Use that function to find the node(s) that has the highest betweenness centrality in the network: top_bc
+    # top_bc = find_node_with_highest_bet_cent(T)
+    # print(top_bc)
+    # Write an assertion statement that checks that the node(s) is/are correctly identified.
+    # for node in top_bc:
+    #     assert nx.betweenness_centrality(T)[node] == max(nx.betweenness_centrality(T).values())
+
     # Compute betweenness centrality: bet_cent
-    bet_cent = nx.betweenness_centrality(G)
+    # bet_cent = nx.betweenness_centrality(G)
+    # experimenting betweenness_centrality_parallel
+    bet_cent = betweenness_centrality_parallel(G)
 
     # Compute maximum betweenness centrality: max_bc
     max_bc = max(list(bet_cent.values()))
-
     nodes = set()
-
     # Iterate over the betweenness centrality dictionary
     for k, v in bet_cent.items():
-
         # Check if the current value has the maximum betweenness centrality
         if v == max_bc:
             # Add the current node to the set of nodes
             nodes.add(k)
-
     return nodes
-
-
-# Use that function to find the node(s) that has the highest betweenness centrality in the network: top_bc
-# top_bc = find_node_with_highest_bet_cent(T)
-# print(top_bc)
-# Write an assertion statement that checks that the node(s) is/are correctly identified.
-# for node in top_bc:
-#     assert nx.betweenness_centrality(T)[node] == max(nx.betweenness_centrality(T).values())
 
 
 def is_in_triangle(G, n):
@@ -222,7 +214,7 @@ def maximal_cliques(G, size):
 # assert len(maximal_cliques(T, 3)) == 33
 
 
-def get_nodes_and_nbrs(G, nodes_of_interest):
+def get_nodes_and_neighbors(G, nodes_of_interest):
     """
     Returns a subgraph of the graph `G` with only the `nodes_of_interest` and their neighbors.
     """
