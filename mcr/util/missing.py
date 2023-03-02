cf4af190f9f0a313875d2191d25b47dd83439661
Maurício Collaça <mauriciocramos@gmail.com>
2023-03-02 15:12:07 -0300

    TODO

--- mcr/util/missing.py
+++ mcr/util/missing.py
@@ -2,13 +2,14 @@ from matplotlib import pyplot as plt
 
 
 def plot_missing(df, figsize=None, xlim=None, decimals=2):
+    # TODO: stack column and overall bars annotating only the count
     missing = missing_report(df, ascending=True)
     if figsize is None:
         figsize = (10, missing.shape[0]/4)
     if xlim is None:
         xlim = [0, missing['missing'].max()*1.3]
     ax = missing['missing'].plot(kind='barh', figsize=figsize, xlim=xlim)
-    labels = [f"{row[0]:,.0f} ({row[1]*100:.{decimals}f}%/{row[2]*100:.{decimals}f}%)" for row in missing.values]
+    labels = [f"{row[0]:,.0f} p={row[1]*100:.{decimals}f}% ovr={row[2]*100:.{decimals}f}%" for row in missing.values]
     ax.bar_label(ax.containers[0], labels=labels)
     plt.show()
 
