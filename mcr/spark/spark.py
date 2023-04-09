7cec91a267c0f44e43e6ae556bb8f623c444b812
Maurício Collaça <mauriciocramos@gmail.com>
2023-04-09 04:05:56 -0300

    split_explode_join and prefixed_join

--- mcr/spark/spark.py
+++ mcr/spark/spark.py
@@ -1,3 +1,5 @@
+import datetime
+
 def min_max_scaler(df, cols_to_scale):
     # Takes a dataframe and list of columns to minmax scale. Returns a dataframe.
     for col in cols_to_scale:
@@ -22,3 +24,37 @@ def column_dropper(df, threshold):
         if missing_percent > threshold:
             df = df.drop(col)
     return df
+
+
+def train_test_split_date(df, split_col, test_days):
+    split_date = None
+    if isinstance(test_days, float):
+        max_date = df.agg({split_col: 'max'}).collect()[0][0]
+        min_date = df.agg({split_col: 'min'}).collect()[0][0]
+        split_in_days = int((max_date - min_date).days * 0.8)
+        split_date = min_date + datetime.timedelta(days=split_in_days)
+    elif isinstance(test_days, int):
+        max_date = df.agg({split_col: 'max'}).collect()[0][0]
+        split_date = max_date - datetime.timedelta(days=test_days)
+        max_date = df.agg({split_col: 'max'}).collect()[0][0]
+    return split_date
+
+
+def split_explode_join(df, groupby, column, sep=', ', concat_sep='_'):
+    exploded_df = df[[groupby, column]]\
+        .fillna('NaN', subset=column)\
+        .withColumn(f'{column}_LIST' , F.split(F.upper(column), sep))\
+        .withColumn(f'EXPLODED_{column}_LIST', F.explode(f'{column}_LIST'))\
+        .withColumn(f'EXPLODED_{column}_LIST', F.concat(F.lit(f'{column}{concat_sep}'), F.trim(f'EXPLODED_{column}_LIST')))\
+        .withColumn('ONE', F.lit(1))\
+        .groupBy(groupby).pivot(f'EXPLODED_{column}_LIST').agg(F.coalesce(F.first('ONE')))
+    return df.join(exploded_df, on='NO', how='left')
+
+
+def prefixed_join(df, groupby, column, concat_sep=':'):
+    prefixed_df = df[[groupby, column]]\
+        .dropna(subset=column)\
+        .withColumn(f'{column.upper()}_PREFIXED', F.concat(F.lit(f'{column}{concat_sep}'), F.upper(F.trim(column))))\
+        .withColumn('ONE', F.lit(1))\
+        .groupBy(groupby).pivot(f'{column}_PREFIXED').agg(F.coalesce(F.first('ONE')))
+    return df.join(prefixed_df, on='NO', how='left')
