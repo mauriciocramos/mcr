88193e55646cea14476eae3692910fc67af4621e
maurício <mauriciocramos@gmail.com>
2025-04-12 23:28:27 -0300

    minor bug

--- mcr/torch/torch.py
+++ mcr/torch/torch.py
@@ -25,19 +25,18 @@ class CustomImageDataset(Dataset):
             label = self.target_transform(label)
         return image, label
 
-def create_sequences(data, seq_length):
-    xs, ys = [], []
-    # Iterate over data indices
 
-    # for i in range(len(data) - seq_length):
-    # changed above line to avoid incomplete sequences
-    # for i in range( int(len(data)/seq_length)*seq_length - seq_length):
-    # changed again
-    for i in range( len(data) // seq_length * seq_length - seq_length):
-        # Define inputs
-        x = data[i:i+seq_length, 1]
-        # Define target
-        y = data[i+seq_length, 1]
+def create_sequences(data, seq_length, y_col_idx):
+    """
+    Generates input sequences and corresponding target values for time-series data.
+    Parameters: input data, number of time steps in each sequence, target column name.
+    Returns: NumPy arrays for input sequences an target values.
+    """
+    xs, ys = [], []
+    for i in range(len(data) - seq_length):
+    # for i in range(len(data) // seq_length * seq_length - seq_length):
+        x = data[i:(i+seq_length)]
+        y = data[i+seq_length, y_col_idx]
         xs.append(x)
         ys.append(y)
     return np.array(xs), np.array(ys)
